59eb5c0f3329881010d6d9e36c418b6eb8fbf354
Priyambada216 <144552804+Priyambada216@users.noreply.github.com>
2023-12-08 18:38:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 function isSameType(value1, value2) {
-  if (isNaN(value1) && isNaN(value2)) {
-    return true;
-  }
+  it("should return false for different types", () => {
+  const value1 = "123"; // String type
+  const value2 = 123;    // Number type
 
-  return typeof value1 === typeof value2;
+  cy.visit(baseUrl, {
+    onBeforeLoad(win) {
+      // Stub your functions here
+      cy.stub(win, "prompt").onFirstCall().returns(value1).onSecondCall().returns(value2);
+    }
+  });
+
+  cy.on("window:alert", str => {
+    expect(str.toString()).to.equal("false");
+  });
+});
+
 }
 
 // do not change the code below.
